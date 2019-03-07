5b3b86ab69f9d7d94e83eaa5ad7f67bbaa672290
Brandon Gregory <bgthemanofsteel@gmail.com>
2019-03-07 15:00:18 -0600

    playing with nav buttons

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+let number = 0;
 
 $('.menu0').on('click', function(){
     $('.slide0').slideDown();
     $('.slide1').hide();
     $('.slide2').hide();
     $('.slide3').hide();
+    number =0;
 })
 
 $('.menu1').on('click', function(){
@@ -11,6 +13,7 @@ $('.menu1').on('click', function(){
     $('.slide2').hide();
     $('.slide3').hide();
     $('.slide1').slideDown();
+    number = 1;
 })
 
 $('.menu2').on('click', function(){
@@ -18,6 +21,7 @@ $('.menu2').on('click', function(){
     $('.slide1').hide();
     $('.slide3').hide();
     $('.slide2').slideDown();
+    number =2;
 })
 
 $('.menu3').on('click', function(){
@@ -25,4 +29,14 @@ $('.menu3').on('click', function(){
     $('.slide1').hide();
     $('.slide2').hide();
     $('.slide3').slideDown();
+    number = 3;
+})
+
+
+
+$('.next').on('click', function(){
+    $('.slide' + number).animate({width:'toggle'},500).delay(2000);
+    $('.menu' + number).removeClass('current');
+    $('.slide' + (number +1)).animate({width:'toggle'},500).delay(2000);
+    $('.menu' + (number+1)).removeClass('current');
 })
