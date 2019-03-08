b5def480d5f446eab18ccb437c1a878808a2d760
Brandon Gregory <bgthemanofsteel@gmail.com>
2019-03-08 09:31:55 -0600

    more css fixes

--- index.js
+++ index.js
@@ -2,33 +2,25 @@ let number = 0;
 
 $('.menu0').on('click', function(){
     $('.slide' + number).slideUp();
-    $('.menu' + number).removeClass('current');
     $('.slide0').slideDown();
-    $(".menu0").addClass('current');
     number =0;
 })
 
 $('.menu1').on('click', function(){
     $('.slide' + number).slideUp();
-    $('.menu' + number).removeClass('current');
     $('.slide1').slideDown();
-    $(".menu1").addClass('current');
     number = 1;
 })
 
 $('.menu2').on('click', function(){
     $('.slide' + number).slideUp();
-    $('.menu' + number).removeClass('current');
     $('.slide2').slideDown();
-    $(".menu2").addClass('current');
     number =2;
 })
 
 $('.menu3').on('click', function(){
     $('.slide' + number).slideUp();
-    $('.menu' + number).removeClass('current');
     $('.slide3').slideDown();
-    $(".menu3").addClass('current');
     number = 3;
 })
 
@@ -36,15 +28,11 @@ $('.menu3').on('click', function(){
 
 $('.next').on('click', function(){
     $('.slide' + number).slideUp();
-    $('.menu' + number).removeClass('current');
     $('.slide' + (number +1)).slideDown();
-    $('.menu' + (number+1)).addClass('current');
 
     if (number === 3){
         $(".slide3").slideUp();
-        $(".menu3").removeClass('current');
         $(".slide0").slideDown();
-        $(".menu0").addClass('current');
         $("#next").css('transform', 'rotate(180deg)');
 
         number = -1;
